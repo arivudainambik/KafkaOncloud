d407f99278489ef24bd93ed886c8ec8be136ec94
arivudainambik <43051902+arivudainambik@users.noreply.github.com>
2018-09-28 11:53:42 -0500

    first checkin

---
+++ SpringBootDockerAzure/src/main/java/com/rishu/workset/SpringBootDockerAzureApplication.java
@@ -0,0 +1,12 @@
+package com.rishu.workset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerAzureApplication.class, args);
+	}
+}
---
+++ SpringBootDockerAzure/src/test/java/com/rishu/workset/SpringBootDockerAzureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rishu.workset;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDockerAzureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
